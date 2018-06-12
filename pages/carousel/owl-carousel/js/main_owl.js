887496f2e5baad07dd1368978a678f3e12e02e2f
utshab42 <utshab.roy@gmail.com>
2018-06-12 17:29:21 +0600

    basic demo implemented for owl

---
+++ pages/carousel/owl-carousel/js/main_owl.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function ($) {
+    //for basic usage
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+});
+
