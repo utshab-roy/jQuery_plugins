8325a5abdc27360d881a5eaf4c8d2001253f7f52
utshab <utshab.roy@gmail.com>
2018-06-13 02:07:43 +0600

    owl-carousel mousewheel demo completed.

--- pages/carousel/owl-carousel/js/main_owl.js
+++ pages/carousel/owl-carousel/js/main_owl.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function ($) {
     //for basic usage
-    $('.owl-carousel').owlCarousel({
+    var basic_owl = $('.basic_slider');
+    basic_owl.owlCarousel({
         loop:true,
         margin:10,
         nav:true,
@@ -16,5 +17,53 @@ jQuery(document).ready(function ($) {
             }
         }
     });
+
+    //for mousewheel usage
+    var mousewheel_owl = $('.mousewheel_slider');
+    mousewheel_owl.owlCarousel({
+        loop:true,
+        margin:10,
+        mouseDrag:false,
+        autoHeight:true,
+        responsiveClass:true,
+        autoplay:true,
+        autoplayTimeout:6000,
+        autoplayHoverPause:true,
+        merge:true,
+        smartSpeed: 1000,
+        animateOut: 'rotateOut',
+        autoplaySpeed:5000,
+
+        responsive:{
+            0:{
+                items:1,
+                nav:true,
+                loop:true
+            },
+            600:{
+                items:3,
+                nav:false,
+                loop:false,
+                mergeFit:true
+            },
+            1000:{
+                items:3,
+                nav:true,
+                navText: ['Prev','Next'],
+                loop:true,
+                mergeFit:false
+            }
+        }
+    });
+
+    mousewheel_owl.on('mousewheel', '.owl-stage', function (e) {
+        if (e.deltaY>0) {
+            mousewheel_owl.trigger('next.owl');
+        } else {
+            mousewheel_owl.trigger('prev.owl');
+        }
+        e.preventDefault();
+    });
+
 });
 
